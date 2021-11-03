026621c2b008a8353d23403c8b4cd8d3915f3705
Daniel Lopez <dlopezre95@gmail.com>
2021-11-03 17:21:04 +0100

    movement added to soldier class

--- js/characters.js
+++ js/characters.js
@@ -1,22 +1,75 @@
 class Soldier {
-    constructor (hp, direction, weapon){
-    this.hp = hp;
+    constructor (posX= 0, posY = 0, direction ="N", hp, weapon, ammo){
+    this.posX = posX;
+    this.posY = posY;
     this.direction = direction;
+    this.hp = hp;
     this.weapon = weapon;
+    this.ammo = ammo;
     }
-    //Methods 
-    _attack()
-    _reload()
-    _move()
+    attack()
+    reload()
+    Forward(){
+        switch (this.direction) {
+            case "N":
+                this.posY--;
+                break;
+            case "E":
+                this.posX++;
+                break;
+            case "S":
+                this.posY++;
+                break;
+            case "W":
+                this.posY--;
+                break;
+        }   
+    }
+    Left() {
+        switch (this.direction) {
+          case "N":
+            this.direction = "W";
+            break;
+          case "E":
+            this.direction = "N";
+            break;
+          case "S":
+            this.direction = "E";
+            break;
+          case "W":
+            this.direction = "S";
+            break;
+          default:
+            break;
+        }
+    }
+    Right() {
+        switch (this.direction) {
+          case "N":
+            this.direction = "E";
+            break;
+          case "E":
+            this.direction = "S";
+            break;
+          case "S":
+            this.direction = "W";
+            break;
+          case "W":
+            this.direction = "N";
+            break;
+          default:
+            break;
+        }
+    }
 }
 class Hero extends Soldier {
-    constructor (hp, direction, weapon, level){
-        super(hp,direction, weapon);
+    constructor (posX= 50, posY = 50, direction ="N", hp, weapon, ammo, level){
+        super(posX, posY, direction, hp, weapon, ammo);
         this.level = level;
     }
-    _changeWeapon()
+    changeWeapon()
 }
-class Enemy extends Soldier {
+/*class Enemy extends Soldier {
     constructor (hp, direction, weapon){
         super(hp,direction, weapon);
     }
@@ -34,3 +87,4 @@ class Boss extends Soldier {
     }
 }
 const Stan = new Boss (`Stan the Razor`, 50,` `, `Laser Gun`, `I enjoy the Killing`);
+*/
