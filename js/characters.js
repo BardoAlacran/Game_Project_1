998d358053257dfe975c7d01838bee2594827209
Daniel Lopez <dlopezre95@gmail.com>
2021-11-03 16:04:17 +0100

    classes added on char js

--- js/characters.js
+++ js/characters.js
@@ -1,1 +1,36 @@
-3
+class Soldier {
+    constructor (hp, direction, weapon){
+    this.hp = hp;
+    this.direction = direction;
+    this.weapon = weapon;
+    }
+    //Methods 
+    _attack()
+    _reload() 
+    _move()
+}
+class Hero extends Soldier {
+    constructor (hp, direction, weapon, level){
+        super(hp,direction, weapon);
+        this.level = level;
+    }
+    _changeWeapon()
+}
+class Enemy extends Soldier {
+    constructor (hp, direction, weapon){
+        super(hp,direction, weapon);
+    }
+}
+class Boss extends Soldier {
+    constructor (name, hp, direction, weapon, passive){
+        super(hp,direction, weapon);
+        this.name = name;
+        this.passive = passive;
+    }
+    _changeweapon()
+    _dash()
+    _recover(){
+        //every 10s heals himself 5hp
+    }
+}
+const Stan = new Boss (`Stan the Razor`, 50,` `, `Laser Gun`, `I enjoy the Killing`);
