b6c67894b5a307231140643b6d0fbea59162c7a1
Daniel Lopez <dlopezre95@gmail.com>
2021-11-04 16:45:42 +0100

    cotroles added

--- js/game.js
+++ js/game.js
@@ -1,1 +1,47 @@
-2
+class Game {
+    constructor(sets){
+        this.ctx = sets.ctx;
+        this.hero = sets.hero;
+    }
+    _drawHero(){
+        this.ctx.fillStyle = 'blue';
+        this.ctx.fillRect(150, 150, 10, 10);
+    }
+    _drawEnemyes(){
+        this.ctx.fillStyle = 'red';
+        this.ctx.fillRect(, , 10, 10)
+        //los enemigos estarán posicionados en sitios concretos para que no coincidan con los elementos de mapa.
+    }
+    _drawElements(){
+        this.ctx.fillStyle = 'brown';
+        
+        //quiero hacer elementos que sean cuadrados, así juntar algunos para hacer paredes, o hacer otras cosas. Los elementos no estarán posicionados aleatoriamente.
+    }
+    _controls(){
+        document.addEventListener('keydown', (event) =>{
+            switch (event.code) {
+                case "ArrowUp":
+                    this.hero.Up
+                    break;
+                case "ArrowDown":
+                    this.hero.Down
+                    break;
+                            
+                case "ArrowLeft":
+                    this.hero.Left
+                    break;
+                                    
+                case "ArrowRight":
+                    this.hero.Right
+                    break;
+                    default:
+                    break;
+            }
+        })
+        
+    }
+    start(){
+        this._controls();
+        
+    }
+}
--- js/main.js
+++ js/main.js
@@ -2,14 +2,30 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     function splashScreen (){
         const splash = document.querySelector('splash');
-        splash.innerHTML = `
-        <div>
-            <button id="play">Enter the Journey</button>
-        </div>`;
-
+        
         const play = document.querySelector('#play');
-
-        const start = document.querySelector('#start');
+        play.addEventListener('click' () => {
+            const game = document.querySelector('#game');
+        
+        const start = game.querySelector('#start');
+            start.addEventListener('click' () =>{
+                const canvas = document.querySelector('#canvas');
+                const ctx = canvas.getContext('2d'); 
+            })
+        })
     }
-})
+    splashScreen(); 
+});
 
+/*const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
+
+
+ctx.fillStyle = 'purple';
+ctx.fillRect(10, 10, 50, 50)
+
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'purple';
+ctx.fillRect(10, 10, 50, 50);*/
