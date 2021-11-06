a13a1fdf918e15d9365b89b410bf689d50c69a21
Daniel Lopez <dlopezre95@gmail.com>
2021-11-06 14:39:14 +0100

    player printed

--- js/game.js
+++ js/game.js
@@ -1,36 +1,41 @@
 class Game {
     constructor(options, callback){
         this.ctx = options.ctx;
-        this.player = Player;
+        this.player = options.player;
         this.rows = options.rows;
         this.columns = options.columns;
-        this.map = map;
+        this.map = options.map;
         this.callback = callback;
     }
     _clean(){
-        this.ctx.clearRect(0, 0, 500, 500);
+        this.ctx.fillStyle = "white",
+        
+        this.ctx.fillRect(0, 0, 500, 500);
+
     }
 
     _drawPlayer(){
         this.ctx.fillStyle = 'pink';
+        console.log(this.player);
         this.ctx.fillRect(this.player.posY, this.player.posX, 50, 50);
     }
 
     _drawWorld() {
+        console.log('world') 
         this.map.forEach(arrayRows => {
             arrayRows.forEach(arrayColumns => {
                 switch (arrayColumns) {
-                    case "r":
+                    /*case "r":
                         this.ctx.fillStyle = 'brown';
-                        this.ctx.fillRect(arrayRows * 50, arrayColumns * 50, 50, 50);
+                        this.ctx.fillRect(this.map.arrayRows * 50, this.map.arrayColumns * 50, 50, 50);
                         break;
                     case "f":
-                        this.ctx.fillStyle = 'grey';
-                        this.ctx.fillRect(arrayRows * 50, arrayColumns * 50, 50, 50);
-                        break;
+                        this.ctx.fillStyle = 'grey'
+                        this.ctx.fillRect(this.rows * 50, this.columns * 50, 50, 50);
+                        break;*/
                     case "d":
                         this.ctx.fillStyle = 'green';
-                        this.ctx.fillRect(arrayRows * 50, arrayColumns * 50, 50, 50);
+                        this.ctx.fillRect( * 50,  * 50, 50, 50);
                     default:
                         break;
                 }                
@@ -61,17 +66,18 @@ class Game {
         });
     }
     _update(){
-        this._clean();
-        this._drawPlayer();
-        this._drawWorld();
+        this._clean()
+        this._drawWorld()
+        this._drawPlayer()
         //añadir todas las functiones de Game.
 
-
+        
         window.requestAnimationFrame(this._update.bind(this));
     };
 
     start() {
         this._controls()
         window.requestAnimationFrame(this._update.bind(this));
+        
     }
 };
--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-document.addEventListener('DOMContentLoaded', () =>{})
-function splashScreen () {
-    const playButton = document.querySelector('#play');
-    playButton.addEventListener('click', () =>{
-        const splash = document.querySelector('#splash');
-        splash.classList.add('hidden');
-        const game = document.querySelector('#game');
-        game.classList.remove('hidden');
-    });
-
+document.addEventListener('DOMContentLoaded', () => { 
+    function splashScreen() {
+        const playButton = document.querySelector('#play');
+        playButton.addEventListener('click', () => {
+            const splash = document.querySelector('#splash');
+            splash.classList.add('hidden');
+            const game = document.querySelector('#game');
+            game.classList.remove('hidden');
+        });
+    
         const startButton = document.querySelector('#start');
-        startButton.addEventListener('click', ()=>{
-
+        console.log(startButton)
+        startButton.addEventListener('click', () => {
+            console.log('click')
             const canvas = document.querySelector('#canvas');
             const ctx = canvas.getContext('2d');
             const gaiaGame = new Game(
@@ -19,23 +20,30 @@ function splashScreen () {
                     rows: canvas.width / 10,
                     columns: canvas.height / 10,
                     map: map,
-                    player: Player,
+                    player: new Player(5, canvas.width / 10, canvas.height / 10, 50, 50),
                 },
-            );   
-            
-            
+            );
+            gaiaGame.start()
+    
             const gameover = document.querySelector('#gameover');
             gameover.classList.remove('hidden');
         });
     
         const gameoverButton = document.querySelector('#gameoverButton');
-        gameoverButton.addEventListener('click', ()=>{
+        gameoverButton.addEventListener('click', () => {
             splash.classList.remove('hidden');
             gameoverButton.classList.add('hidden');
-            
-        }); 
-};
-splashScreen();
+    
+        });
+    
+    
+    
+    };
+    
+    splashScreen();
+    console.log("load");
+})
+
 //ahora a la segunda vez que sale start, no aparece game over button. He de intentar que sí aparezca y la función sea un bucle.
 
 /*[['G','R','G'],['G','G','G'],['G','G','G']]*/
